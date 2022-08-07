4e6a9d6cf6212a967f737fd112ccaac37d827d3c
Melisa Im <melisaevelynim@gmail.com>
2022-08-06 21:07:49 -0700

    drawing with characters

--- sketch.js
+++ sketch.js
@@ -23,13 +23,14 @@ const sketch = () => {
         // work in uv space
         const u = count <= 1 ? 0.5 : x / (count - 1); // value between 0 and 1
         const v = count <= 1 ? 0.5 : y / (count - 1); 
-        const radius = Math.abs(random.noise2D( u, v )) * 0.05; 
+        const radius = Math.abs(random.noise2D( u, v )) * 0.2; 
         points.push({
           // radius: random.value() * 0.02,
           // radius: Math.abs(0.01 + random.gaussian() * 0.01), // more organic distribution
           radius,
           position: [ u, v ],
-          color: random.pick(palette)
+          color: random.pick(palette),
+          rotation: random.noise2D( u, v )
         }); 
       }
     }
@@ -42,26 +43,36 @@ const sketch = () => {
   // random.setSeed(201);
   const points = createGrid().filter(() => random.value() > 0.5); // randomly remove points
 
-  const margin = 200; 
+  const margin = 400; 
   
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect( 0, 0, width, height );
 
     points.forEach(data => {
-      const {radius, position: [u, v], color} = data;
+      const {radius, position: [u, v], color, rotation} = data;
       // const x = u * width;
       // const y = v * height;
       const x = lerp(margin, width - margin, u);
       const y = lerp (margin, height - margin, v);
 
-      context.beginPath();
-      context.arc( x , y, radius * width, 0, Math.PI * 2, false);
+      // context.beginPath();
+      // context.arc( x , y, radius * width, 0, Math.PI * 2, false);
       // context.strokeStyle = 'black';
       // context.lineWidth = 20;
       // context.stroke();
+
+      // context.fillStyle = color;
+      // context.fill();
+
+      // draw text instead
+      context.save();
       context.fillStyle = color;
-      context.fill();
+      context.font = `${radius * width}px "Arial"`;
+      context.translate(x, y);
+      context.rotate(rotation);
+      context.fillText('#', 0, 0);
+      context.restore();
     }); 
   };
 };
