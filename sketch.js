c269f30254bea4b7a695f46e5030ca9b1befc256
Melisa Im <melisaevelynim@gmail.com>
2022-08-06 19:19:36 -0700

    palette

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const canvasSketch = require('canvas-sketch');
 const { lerp } = require('canvas-sketch-util/math'); // linear interpolation 
 const random = require('canvas-sketch-util/random'); // pseudo random number generation 
+const palettes = require('nice-color-palettes');
 
 const settings = {
   dimensions: [2048, 2048]
@@ -10,17 +11,23 @@ const settings = {
 };
 
 const sketch = () => {
+  const colorCount = random.rangeFloor(1, 6); // 5 inclusive
+  // const palette = random.pick(palettes)
+  const palette = random.pick(palettes).slice(0, colorCount);
+
   const createGrid = () => {
     const points = [];
-    const count = 40;
+    const count = 30;
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         // work in uv space
         const u = count <= 1 ? 0.5 : x / (count - 1); // value between 0 and 1
         const v = count <= 1 ? 0.5 : y / (count - 1); 
         points.push({
-          radius: random.value() * 0.02,
-          position: [ u, v ]
+          // radius: random.value() * 0.02,
+          radius: Math.abs(0.01 + random.gaussian() * 0.01), // more organic distribution
+          position: [ u, v ],
+          color: random.pick(palette)
         }); 
       }
     }
@@ -40,7 +47,7 @@ const sketch = () => {
     context.fillRect( 0, 0, width, height );
 
     points.forEach(data => {
-      const {radius, position: [u, v]} = data;
+      const {radius, position: [u, v], color} = data;
       // const x = u * width;
       // const y = v * height;
       const x = lerp(margin, width - margin, u);
@@ -51,7 +58,7 @@ const sketch = () => {
       // context.strokeStyle = 'black';
       // context.lineWidth = 20;
       // context.stroke();
-      context.fillStyle = 'red';
+      context.fillStyle = color;
       context.fill();
     }); 
   };
