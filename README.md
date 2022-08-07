1887f16e22ad0ac2eb3daa3b2fb5a4c98c30d027
Melisa Im <melisaevelynim@gmail.com>
2022-08-06 18:47:09 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Melisa's Generative Art Practice
+
+- Following along on a Frontend Master's course called [Creative Coding with Canvas & WebGL](https://frontendmasters.com/courses/canvas-webgl/) with [Matt DesLauriers](https://www.mattdesl.com/)
---
+++ sketch.js
@@ -0,0 +1,49 @@
+const canvasSketch = require('canvas-sketch');
+const { lerp } = require('canvas-sketch-util/math'); // linear interpolation 
+
+const settings = {
+  dimensions: [2048, 2048]
+  // dimensions: 'A4',   // or any other common print sizes
+  // units: 'in',    // or 'cm'
+  // pixelsPerInch: 300 // by default it is 72
+};
+
+const sketch = () => {
+  const createGrid = () => {
+    const points = [];
+    const count = 5;
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        // work in uv space
+        const u = count <= 1 ? 0.5 : x / (count - 1); // value between 0 and 1
+        const v = count <= 1 ? 0.5 : y / (count - 1); 
+        points.push([ u, v ]); 
+      }
+    }
+    return points;
+  }
+  
+  const points = createGrid();
+  const margin = 400; 
+  
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect( 0, 0, width, height );
+
+    points.forEach(([u,v]) => {
+      // const x = u * width;
+      // const y = v * height;
+      const x = lerp(margin, width - margin, u);
+      const y = lerp (margin, height - margin, v);
+
+      context.beginPath();
+      context.arc( x , y, 100, 0, Math.PI * 2, false);
+      context.strokeStyle = 'black';
+      context.lineWidth = 40;
+      context.stroke();
+    }); 
+  };
+};
+
+canvasSketch(sketch, settings);
+ 
